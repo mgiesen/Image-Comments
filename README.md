e801aee81993dc7d260a4c993e22a3eb3eafc1eb
Maximilian Giesen <maximilian.giesen@sls.siempelkamp.com>
2023-09-26 01:01:49 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Image-Comments
+# Image Comments for Visual Studio Code
+
+## Description
+
+The `Image Comments` Visual Studio Code extension lets you easily add visual comments such as sketches or diagrams directly into your source code, improving code documentation and understanding.
+
+![Image](readme/image-comment.png)
+
+## Features
+
+- Supports multiple programming languages: JavaScript, Python, C++, C#
+- Hover over comments to display your custom images in a tooltip
+- Easy-to-use comment syntax for linking images
+
+## Requirements
+
+- Visual Studio Code version 1.60.0 or higher
+
+## Installation
+
+1. Open Visual Studio Code
+2. Go to Extensions Marketplace
+3. Search for "Image Comments"
+4. Click the "Install" button
+
+## Usage
+
+Write your comment in the following format:
+
+```plaintext
+// Describes the function logic [image-comments/image1.png]
+```
+
+-Create a dedicated folder named `image-comments` within your project directory and populate it with your images.
+-The comment preceding the image path will be displayed in the tooltip.
+-The image path is relative to the image-comments directory in your workspace.
+
+## Author
+
+Maximilian Giesen
+https://github.com/mgiesen
---
+++ extension.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+const vscode = require('vscode');
+
+function activate(context)
+{
+	const supportedLanguages = {
+		'javascript': '\/\/',
+		'python': '#',
+		'cpp': '\/\/',
+		'csharp': '\/\/'
+	};
+
+	for (const [lang, commentSymbol] of Object.entries(supportedLanguages))
+	{
+		let disposable = vscode.languages.registerHoverProvider(lang, {
+			provideHover(document, position, token)
+			{
+				const line = document.lineAt(position.line).text;
+				const commentPattern = new RegExp(`${commentSymbol}(.+) \\[image-comments\\/(.+)\\]$`);
+
+				const match = line.match(commentPattern);
+				if (match && match[1] && match[2])
+				{
+					const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
+					if (workspaceFolder)
+					{
+						const rootPath = workspaceFolder.uri.fsPath;
+						const imgPath = path.join(rootPath, 'image-comments', match[2]);
+
+						if (fs.existsSync(imgPath))
+						{
+							const imgUri = vscode.Uri.file(imgPath).toString();
+							const hoverContent = [
+								'# Image Comments',
+								match[1],
+								'',
+								`![Image](${imgUri})`
+							].join('\n');
+							const md = new vscode.MarkdownString(hoverContent);
+							return new vscode.Hover(md);
+						}
+					}
+				}
+			}
+		});
+		context.subscriptions.push(disposable);
+	}
+}
+
+exports.activate = activate;
